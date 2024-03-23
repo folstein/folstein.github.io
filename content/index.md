fb10e17a8b7df29dcefc50c407df07ac33d6a84a
Steven Folstein <steve@geodetic.com>
2024-03-22 20:04:53 -0400

    Quartz sync: Mar 22, 2024, 8:04 PM

--- content/index.md
+++ content/index.md
@@ -1,3 +1,7 @@
 ---
 title: Welcome to Folstein
 ---
+
+Folstein is currently a blank Quartz installation.
+
+See the [documentation](https://quartz.jzhao.xyz) for how to get started.
--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "folstein.github.io",
+    baseUrl: "folstein.org",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
